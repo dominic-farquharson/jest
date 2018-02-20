8e79b3cdd0491d3c2c28bc7ad49223093854a61a
Dominic Farquharson <farquharsondominic@gmail.com>
2018-02-20 11:56:55 -0500

    add comments

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,7 @@
 const sum = require('./sum');
 
 test('adds 1 + 2 to equl 3', () => {
- expect(sum(1,2)).toBe(3);
+  // expect - returns an expectation object
+  // toBe - matcher that can be called on the expectation object
+  expect(sum(1,2)).toBe(3);
 });
